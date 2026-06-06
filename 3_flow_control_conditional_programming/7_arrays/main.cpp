d70f560801fcce218b671f1bff0f2077530d3c53
Javier Toro <javier.toro.b@ug.uchile.cl>
2026-06-06 12:36:39 -0400

    3.7 arrays complete

--- 3_flow_control_conditional_programming/7_arrays/main.cpp
+++ 3_flow_control_conditional_programming/7_arrays/main.cpp
@@ -51,5 +51,45 @@ int main(){
 	}
 
 	std::cout << scores << std::endl;
+	
+	//Declare and initialize at the same time
+	double salaries[5] {12.7, 7.5, 13.2, 8.1, 9.3};
+
+	for (size_t i{0}; i < 5; ++i){
+		std::cout << "salary[" << i << "]: " << salaries[i] << std::endl;
+	}
+
+	//If you don't initialize all the elements, those you leave out
+	//are initialized to 0
+	
+	int families[5] {12, 7, 5};
+
+	for (size_t i{0}; i < 5; ++i){
+		std::cout << "families[" << i << "]: " << families[i] << std::endl;
+	}
+
+	// omit the size of the array at declaration
+	// it will infer size from the number of elements when initializing.
+	
+	int class_size[] {10,12,15,11,18,17};
+
+	for (auto value : class_size){ //range-based for loop
+		std::cout <<"value :" << value << std::endl;
+		// we can iterate values in arrays instead of index.
+	}
+
+	// Non-modifiable array.
+        const int birds[] {10,12,15,11,18,17};
+
+	// Operations on arrays
+	int scores_2[] {2,5,8,2,5,6,9};
+	int sum{0};
+
+	for( int element: scores_2){
+		sum += element;
+	}
+	std::cout << "Scores sum : " << sum << std::endl;
+	
+
 	return 0;
 }
